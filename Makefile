dce66d5cc9c0966abaf692f205b77b86359d688a
Martin Petráček <martin.petracek@nic.cz>
2018-05-15 17:05:00 +0200

    refactored to handle disconnects and keep-alive messages

--- Makefile
+++ Makefile
@@ -1,5 +1,5 @@
 all: proxy.c
-	$(CC) proxy.c $(CXXFLAGS) $(LDFLAGS) -lpaho-mqtt3cs -lczmq -lz -lssl -lcrypto -o sentinel-proxy
+	$(CC) proxy.c $(CFLAGS) $(LDFLAGS) -lpaho-mqtt3cs -lczmq -lz -lssl -lcrypto -o sentinel-proxy
 
 clean:
 	rm -f sentinel-proxy
--- proxy.c
+++ proxy.c
@@ -11,14 +11,13 @@
 #define MAX_TOPIC_LEN 256
 #define MAX_MSG_SIZE 1024*1024*2
 #define MAX_WAITING_MESSAGES 50
+//mandatory prefix, that each ZMQ msg (topic) must have (is discarded elsewhere)
+#define ZMQ_TOPIC_PREFIX "sentinel/collect/"
 //0 is no compression, 1 is lowest (fastest) compression, 9 is biggest (slowest) compression, 6 is default
 #define COMPRESS_LEVEL 9
 //QoS levels - see here: https://www.hivemq.com/blog/mqtt-essentials-part-6-mqtt-quality-of-service-levels
 #define MQTT_QOS 0
-
-MQTTClient_connectOptions conn_opts = MQTTClient_connectOptions_initializer;
-MQTTClient_SSLOptions ssl_opts = MQTTClient_SSLOptions_initializer;
-int mqtt_reconnect_wait = 0;
+#define MQTT_KEEPALIVE_INTERVAL 60 //seconds
 
 char * get_name_from_cert(const char * filename){
     //get alternative name from X509 certificate
@@ -50,32 +49,25 @@ void compress_input(const char * in_buf, unsigned long in_len, unsigned char * o
 }
 
 void verify_exists (const char * filename) {
-    struct stat buffer;   
-    if (stat (filename, &buffer) != 0){
+    struct stat tmp;
+    if (stat (filename, &tmp) != 0){
         fprintf(stderr, "%s does not exist\n", filename);
         exit(1);
     }
 }
 
-void mqtt_setup(const char * server_cert, const char * client_cert, const char * client_key){
+void mqtt_setup(MQTTClient_connectOptions * conn_opts, const char * server_cert, const char * client_cert, const char * client_key){
     verify_exists(server_cert);
     verify_exists(client_cert);
     verify_exists(client_key);
-    conn_opts.keepAliveInterval = 60;
-    conn_opts.reliable = 0;
-    conn_opts.cleansession = 1;
-    ssl_opts.enableServerCertAuth = 1;
-    ssl_opts.trustStore = server_cert;
-    ssl_opts.keyStore = client_cert;
-    ssl_opts.privateKey = client_key;
-    conn_opts.ssl = &ssl_opts;
-}
-
-void mqtt_try_reconnect(MQTTClient * client){
-    sleep(1+mqtt_reconnect_wait);
-    MQTTClient_connect(*client, &conn_opts);
-    if (MQTTClient_isConnected(*client)) mqtt_reconnect_wait=0;
-    else if(mqtt_reconnect_wait<=1024) mqtt_reconnect_wait*=2;
+    conn_opts->retryInterval = 5;
+    conn_opts->keepAliveInterval = MQTT_KEEPALIVE_INTERVAL;
+    conn_opts->reliable = 0;
+    conn_opts->cleansession = 1;
+    conn_opts->ssl->enableServerCertAuth = 1;
+    conn_opts->ssl->trustStore = server_cert;
+    conn_opts->ssl->keyStore = client_cert;
+    conn_opts->ssl->privateKey = client_key;
 }
 
 int main(int argc, char* argv[]){
@@ -87,73 +79,99 @@ int main(int argc, char* argv[]){
     const char * client_priv_key_file = (argc>5)?argv[5]:"dev-ca/keys/dev-martin-petracek.key";
     printf("connecting to %s, listening on %s\n", upstream_srv, local_socket);
     printf("server certificate %s, client certificate %s, client private key %s\n", server_cert_file, client_cert_file, client_priv_key_file);
+    //get name from certificate
     char * cert_name=get_name_from_cert(client_cert_file);
     if (!cert_name) {
         fprintf(stderr, "can't get name from the certificate - or file not found\n");
         return 1;
     }
     printf("got name from certificate: %s\n", cert_name);
+    //prepare topic - topic to send is topic_to_check+cert_name+'/'+msg_topic
+    //e.g., if topic_to_check is "sentinel/collect/", cert_name is "user" and msg_topic is "flow", topic should be "sentinel/collect/user/flow"
+    //we prepare the fixed part (topic_to_check+cert_name+'/') here, just the msg_topic is copied by the recv_handler.
+    const char * topic_to_check=ZMQ_TOPIC_PREFIX;
+    char topic_to_send[MAX_TOPIC_LEN];
+    strcpy(topic_to_send, topic_to_check);
+    strcpy(topic_to_send+strlen(topic_to_check), cert_name);
+    topic_to_send[strlen(topic_to_check)+strlen(cert_name)]='/';
+    topic_to_send[MAX_TOPIC_LEN-1]=0;
+    unsigned int topic_to_send_prefix_len=strlen(topic_to_check)+strlen(cert_name)+1; //index where the msg_topic shall be appended
+    char * topic_to_send_pos = topic_to_send+topic_to_send_prefix_len;
+    //prepare buffer for compressed data - it's quite big, so we allocate it beforehand
+    //zlib doc: "Upon entry, destLen is the total size of the destination buffer, which must be at least 0.1% larger than sourceLen plus 12 bytes."
+    const size_t compressed_buf_size=((MAX_MSG_SIZE*1001)/1000+12+1);
+    unsigned char * compressed_buf=(unsigned char*)malloc(compressed_buf_size);
+    //MQTT initialization
     MQTTClient client;
-    mqtt_setup(server_cert_file, client_cert_file, client_priv_key_file);
+    MQTTClient_connectOptions conn_opts = MQTTClient_connectOptions_initializer;
+    MQTTClient_SSLOptions ssl_opts = MQTTClient_SSLOptions_initializer;
+    conn_opts.ssl = &ssl_opts;
+    mqtt_setup(&conn_opts, server_cert_file, client_cert_file, client_priv_key_file);
     MQTTClient_create(&client, upstream_srv, cert_name, MQTTCLIENT_PERSISTENCE_NONE, NULL);
-    zsock_t *receiver = zsock_new (ZMQ_PULL);
+    MQTTClient_connect(client, &conn_opts);
+    int mqtt_reconnect_wait = 0;
+    //ZMQ initialization
+    zsock_t * receiver = zsock_new (ZMQ_PULL);
+    assert(receiver);
     zsock_set_maxmsgsize(receiver, MAX_MSG_SIZE);
     zsock_set_rcvhwm(receiver, MAX_WAITING_MESSAGES);
     zsock_bind(receiver, "%s", local_socket);
-    const char * topic_to_check="sentinel/collect/";
-    //zlib doc: "Upon entry, destLen is the total size of the destination buffer, which must be at least 0.1% larger than sourceLen plus 12 bytes."
-    const unsigned int compressed_buf_len = MAX_MSG_SIZE*1.001+12+1;
-    unsigned char * compressed_buf=(unsigned char*)malloc(compressed_buf_len);
-    const unsigned int topic_buf_len = MAX_TOPIC_LEN;
-    char * topic_buf=(char*)malloc(topic_buf_len);
-    strcpy(topic_buf, topic_to_check);
-    strcpy(topic_buf+strlen(topic_to_check), cert_name);
-    topic_buf[strlen(topic_to_check)+strlen(cert_name)]='/';
-    const unsigned int topic_prefix_len=strlen(topic_to_check)+strlen(cert_name)+1;
-    topic_buf[MAX_TOPIC_LEN-1]=0;
-    char * topic_buf_pos = topic_buf+topic_prefix_len;
-    fprintf(stderr, "connected\n");
-    MQTTClient_connect(client, &conn_opts);
+    zpoller_t * poller = zpoller_new(receiver, NULL);
+    assert (poller);
     for(;;){
-        unsigned long compressed_len;
-        zmsg_t * msg = zmsg_recv (receiver);
-        if (!msg) break;
+        zmq_pollitem_t items [] = {{ receiver, 0, ZMQ_POLLIN, 0}};
+        zpoller_wait(poller, (MQTT_KEEPALIVE_INTERVAL+1)*1000);
+        if (zpoller_terminated(poller)) break;
+        if (zpoller_expired(poller)) {
+            MQTTClient_yield();
+            continue;
+        }
+        zmsg_t * msg = zmsg_recv(receiver);
+        if (!msg) {
+            fprintf(stderr, "no msg\n");
+            continue;
+        }
+        if (zmsg_size(msg)!=2){
+            fprintf(stderr, "received mallformed ZMQ message (expected 2 parts, got %d parts) -> ignoring it\n", zmsg_size(msg));
+            goto recv_end;
+        }
         zframe_t * msg_topic = zmsg_first(msg);
         zframe_t * msg_payload = zmsg_last(msg);
-        if (!msg_topic || !msg_payload){
-            fprintf(stderr, "received ZMQ message with less parts then expected (expected 2) -> ignoring it\n");
-            goto loop_end;
-        }
         if (zframe_size(msg_topic)<strlen(topic_to_check) || strncmp(topic_to_check, zframe_data(msg_topic), strlen(topic_to_check))!=0) {
             fprintf(stderr, "topic prefix %.*s doesn't match, ignoring the message -> ignoring it\n", (int)zframe_size(msg_topic), zframe_data(msg_topic));
-            goto loop_end;
+            goto recv_end;
         }
-        if (zframe_size(msg_topic) >= MAX_TOPIC_LEN-topic_prefix_len-1){
+        if (zframe_size(msg_topic) >= MAX_TOPIC_LEN-topic_to_send_prefix_len-1){
             fprintf(stderr, "topic too long -> ignoring it\n");
-            goto loop_end;
+            goto recv_end;
         }
-        strncpy(topic_buf_pos, zframe_data(msg_topic)+strlen(topic_to_check), zframe_size(msg_topic)-strlen(topic_to_check));
-        topic_buf_pos[zframe_size(msg_topic)-strlen(topic_to_check)]=0;
-        while (!MQTTClient_isConnected(client)){
-            fprintf(stderr, "not connected to server, reconnecting...\n");
-            mqtt_try_reconnect(&client);
-        }
-        compressed_len = compressed_buf_len;
+        strncpy(topic_to_send_pos, zframe_data(msg_topic)+strlen(topic_to_check), zframe_size(msg_topic)-strlen(topic_to_check));
+        topic_to_send_pos[zframe_size(msg_topic)-strlen(topic_to_check)]=0;
+        unsigned long compressed_len = compressed_buf_size;
         compress_input(zframe_data(msg_payload), zframe_size(msg_payload), compressed_buf, &compressed_len);
         if (!compressed_len) {
             fprintf(stderr, "compress produced 0 bytes\n");
-            continue;
+            goto recv_end;
         }
-        MQTTClient_publish(client, topic_buf, compressed_len, (char*)compressed_buf, MQTT_QOS, 0, NULL);
-        printf("publishing with topic %s\n", topic_buf);
-    loop_end:
-        zmsg_destroy(&msg);
+        MQTTClient_yield();
+        MQTTClient_deliveryToken dt;
+        while (!MQTTClient_isConnected(client)){
+            fprintf(stderr, "not connected to server, reconnecting...\n");
+            sleep(1+mqtt_reconnect_wait);
+            MQTTClient_connect(client, &conn_opts);
+            if (MQTTClient_isConnected(client)) mqtt_reconnect_wait=0;
+            else if(mqtt_reconnect_wait<=1024) mqtt_reconnect_wait*=2;
+        }
+        int res=MQTTClient_publish(client, topic_to_send, compressed_len, (char*)compressed_buf, MQTT_QOS, 0, &dt);
+        printf("publishing with topic %s - res %d\n", topic_to_send, res);
+        recv_end:
+            zmsg_destroy(&msg);
     }
     MQTTClient_disconnect(client, 0);
     MQTTClient_destroy(&client);
-    zsock_destroy (&receiver);
-    free(compressed_buf);
-    free(topic_buf);
+    zsock_destroy(&receiver);
+    zpoller_destroy(&poller);
     free(cert_name);
+    free(compressed_buf);
     return 0;
 }

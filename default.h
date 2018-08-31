ca90e84a11defbe266beed3ad32adbf50719a06f
Martin Petráček <martin.petracek@nic.cz>
2018-08-31 18:31:58 +0200

    adjusted default paths

--- default.h
+++ default.h
@@ -22,7 +22,7 @@
 #define DEFAULT_UPSTREAM_SRV "ssl://sentinel.turris.cz:1883"
 #define DEFAULT_LOCAL_SOCKET "ipc:///tmp/sentinel_pull.sock"
 #define DEFAULT_CA_FILE "/etc/sentinel/ca.crt"
-#define DEFAULT_CERT_FILE "/etc/sentinel/router.crt"
-#define DEFAULT_KEY_FILE "/etc/sentinel/router.key"
+#define DEFAULT_CERT_FILE "/etc/sentinel/mqtt_cert.pem"
+#define DEFAULT_KEY_FILE "/etc/sentinel/mqtt_key.pem"
 
 #endif /*__SENTINEL_PROXY_DEFAULT_H__*/

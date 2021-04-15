327db063d58a4ec0213ab025342f22d121b12fa8
Miroslav Hanak <miroslav.hanak@nic.cz>
2021-04-15 14:09:38 +0200

    Add logc

---
+++ log.h
@@ -0,0 +1,31 @@
+/*
+ *  Turris:Sentinel Proxy - Main MQTT gateway to Sentinel infrastructure
+ *  Copyright (C) 2018 - 2021 CZ.NIC z.s.p.o. (https://www.nic.cz/)
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+#ifndef __SENTINEL_PROXY_LOG_H__
+#define __SENTINEL_PROXY_LOG_H__
+
+#define DEFLOG log_sentinel_proxy // use our log as default one in logc macros
+
+#include <logc.h>
+#include <logc_assert.h>
+
+#define TRACE_FUNC trace(__func__)
+
+extern log_t log_sentinel_proxy;
+
+#endif /*__SENTINEL_PROXY_LOG_H__*/
+

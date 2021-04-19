4a73e37f791f1a473adde982ed428ac8ee62f1a9
Miroslav Hanak <miroslav.hanak@nic.cz>
2021-04-19 21:34:32 +0200

    Add tests

---
+++ tests/data_collector.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+
+import argparse
+import zmq
+import time
+import random
+import string
+
+
+def run(socket):
+    with zmq.Context() as context, context.socket(zmq.PUSH) as zmq_sock:
+        zmq_sock.connect(socket)
+        while True:
+            first = bytes([random.choice(string.ascii_letters.encode()) for _ in range(5)])
+            second = bytes([random.choice(string.ascii_letters.encode()) for _ in range(5)])
+            print(f"sending data: {first}, {second}")
+            zmq_sock.send_multipart([first, second])
+            time.sleep(3)
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "-s", "--socket", dest="socket_path",
+        default="ipc:///tmp/sentinel_pull.sock",
+        type=str, help="set the socket path"
+    )
+    options = parser.parse_args()
+    print("Data collector running on socket: ", options.socket_path)
+    run(options.socket_path)
+
+
+if __name__ == "__main__":
+    main()

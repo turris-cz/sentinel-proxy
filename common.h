0a70a268268e3ea2692d02db1c3eefadc5243439
Miroslav Hanak <miroslav.hanak@nic.cz>
2021-04-23 23:06:05 +0200

    fixup! fixup! Add utils.h

--- common.h
+++ common.h
@@ -1,10 +1,24 @@
 #include "log.h"
 
+#define TOPIC_PREFIX "sentinel/collect/"
+#define TOPIC_PREFIX_LEN strlen(TOPIC_PREFIX)
+#define ZMQ_MAX_TOPIC_LEN 256
+
+#define TRACE_FUNC TRACE(__func__)
+
 #define CHECK_ERR_LOG(CMD, ...) \
 	do { \
 		if (CMD) { \
 			ERROR(__VA_ARGS__); \
 			return -1; \
+		} \
+	} while (0)
+
+#define CHECK_ERR_VOID_LOG(CMD, ...) \
+	do { \
+		if (CMD) { \
+			ERROR(__VA_ARGS__); \
+			return; \
 		} \
 	} while (0)
 

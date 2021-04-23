aa6befcc7c30c3f8869bbf2faac0a4bcb4e0648a
Miroslav Hanak <miroslav.hanak@nic.cz>
2021-04-23 23:05:39 +0200

    fixup! fixup! fixup! Refactoring

--- sentinel_proxy.c
+++ sentinel_proxy.c
@@ -16,14 +16,15 @@
  *  along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
 
+#include <sys/prctl.h>
 #include <signal.h>
-#include <event2/event.h>
 
 #include "config.h"
-#include "proxy_conf.h"
 #include "proxy_zmq.h"
 #include "proxy_mqtt.h"
-#include "log.h"
+#include "common.h"
+
+#define SENTINEL_PROXY "sentinel-proxy"
 
 static void discard_cb(int severity, const char *msg) {}
 
@@ -34,7 +35,6 @@ static void loopbrake_cb(evutil_socket_t sig, short events, void *arg) {
 
 static void run(const struct proxy_conf *conf) {
 	TRACE_FUNC;
-
 	event_set_log_callback(discard_cb);
 	struct event_base *ev_base = event_base_new(); 
 	struct event *sigint_ev = event_new(ev_base, SIGINT, EV_SIGNAL,
@@ -44,29 +44,27 @@ static void run(const struct proxy_conf *conf) {
 		loopbrake_cb, ev_base);
 	event_add(sigterm_ev, NULL);
 
-	// setup mqtt client
-	// printf("asa\n");
-	// struct proxy_zmq proxy_zmq;
-	// proxy_zmq_init(&proxy_zmq, ev_base, conf->local_socket);
-	
-
 	struct proxy_mqtt mqtt;
 	proxy_mqtt_init(&mqtt, ev_base, conf);
 
-	printf("dispatch\n");
+	struct proxy_zmq proxy_zmq;
+	proxy_zmq_init(&proxy_zmq, ev_base, conf->local_socket);
+	proxy_zmq.mqtt = &mqtt;
+
 	event_base_dispatch(ev_base);
 	
-	event_free(sigint_ev);
+	proxy_zmq_destroy(&proxy_zmq);
+	proxy_mqtt_destroy(&mqtt);
+
 	event_free(sigterm_ev);
-
-	// destroy mqtt client
-
-	// proxy_zmq_destroy(&proxy_zmq);
-
+	event_free(sigint_ev);
 	event_base_free(ev_base);
 }
 
 int main(int argc, char *argv[]) {
+	// set logger and process name for easier debuging
+	prctl(PR_SET_NAME, SENTINEL_PROXY);
+	log_sentinel_proxy->name = SENTINEL_PROXY;
 	struct proxy_conf proxy_conf = {
 		.upstream_srv = DEFAULT_SERVER,
 		.local_socket = DEFAULT_LOCAL_SOCKET,

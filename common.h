6d0e9d44ae4f6747adcb544bcdb816e14548d772
Miroslav Hanak <miroslav.hanak@nic.cz>
2021-05-05 14:16:41 +0200

    Add common.h

---
+++ common.h
@@ -0,0 +1,27 @@
+/*
+ *  Turris:Sentinel Proxy - Main MQTT gateway to Sentinel infrastructure
+ *  Copyright (C) 2018 - 2021 CZ.NIC z.s.p.o. (https://www.nic.cz/)
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+#ifndef __SENTINEL_PROXY_COMMON_H__
+#define __SENTINEL_PROXY_COMMON_H__
+
+#include <string.h>
+
+#define TOPIC_PREFIX "sentinel/collect/"
+#define TOPIC_PREFIX_LEN strlen(TOPIC_PREFIX)
+#define ZMQ_MAX_TOPIC_LEN 256
+
+#endif /*__SENTINEL_PROXY_COMMON_H__*/

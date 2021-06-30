8bf5ef7026d6bd5d31013fa79b13b63a3798eb29
Miroslav Hanak <miroslav.hanak@nic.cz>
2021-06-30 09:51:32 +0200

    proxy_conf: refactoring
    * improve cli options
    * add disable server check option
    * split server option to server and port
    * add more fields to configuration file
    * add logging

--- proxy/proxy_conf.c
+++ proxy/proxy_conf.c
@@ -1,6 +1,6 @@
 /*
  *  Turris:Sentinel Proxy - Main MQTT gateway to Sentinel infrastructure
- *  Copyright (C) 2020 CZ.NIC z.s.p.o. (https://www.nic.cz/)
+ *  Copyright (C) 2018 - 2021 CZ.NIC z.s.p.o. (https://www.nic.cz/)
  *
  *  This program is free software: you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
@@ -16,81 +16,141 @@
  *  along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
 
-#include <stdlib.h>
-#include <argp.h>
-#include <fcntl.h>
 #include <libconfig.h>
-#include <string.h>
-#include <unistd.h>
 #include <logc_argp.h>
+#include <unistd.h>
 
 #include "proxy_conf.h"
 #include "log.h"
 
+#define xstr(s) str(s)
+#define str(s) #s
+
+#define CONF_FIELD_SEREVR "server"
+#define CONF_FIELD_PORT "port"
+#define CONF_FIELD_CL_CERT_FILE "client_cert_file"
+#define CONF_FIELD_CL_KEY_FILE "client_key_file"
+#define CONF_FIELD_CA_CERT_FILE "ca_cert_file"
+#define CONF_FIELD_ZMQ_SOCK_PATH "zmq_socket_path"
+#define CONF_FIELD_DEVICE_TOKEN "device_token"
+
 const char *argp_program_version = PACKAGE_STRING;
 const char *argp_program_bug_address = "<packaging@turris.cz>";
-static char doc[] = "Sentinel:Proxy - Turris:Sentinel data gateway";
-static struct argp_option options[] = {
-	{"server",   'S', "server",       0,  "Sentinel server address" },
-	{"socket",   's', "socket",       0,  "Local ZMQ socket" },
-	{"ca",       'c', "ca_file",      0,  "Path to Sentinel CA file"},
-	{"cert",     'C', "cert_file",    0,  "Path to MQTT cert file"},
-	{"key",      'K', "key_file",     0,  "Path to MQTT key file"},
-	{"token",    't', "device_token", 0,  "Sentinel device token"},
-	{"config",   'f', "config_file",  0,  "Path to config file"},
+static const char doc[] = PACKAGE_NAME " - Turris Sentinel data gateway";
+static const struct argp_option options[] = {
+	{"server", 'S', "server", 0,
+		"Sentinel server address - default is: " DEFAULT_SERVER},
+	{"port", 'p', "port", 0,
+		"Sentinel server port - default is: " xstr(DEFAULT_PORT)},
+	{"zmq-sock", 's', "zmq_socket_path", 0,
+		"ZMQ socket path - default is: " DEFAULT_ZMQ_SOCK_PATH},
+	{"ca-cert", 'c', "ca_cert_file", 0,
+		"Path to Sentinel CA certificate file - default is: " DEFAULT_CA_CERT_FILE},
+	{"cl-cert", 'C', "client_cert_file", 0,
+		"Path to Sentinel client certificate file - default is: " DEFAULT_MQTT_CLIENT_CERT_FILE},
+	{"cl-key", 'K', "client_key_file", 0,
+		"Path to Sentinel client key file - default is: " DEFAULT_MQTT_CLIENT_KEY_FILE},
+	{"token", 't', "device_token", 0, "Sentinel device token"},
+	{"config", 'f', "config_file", 0,
+		"Path to configuration file - default is: " DEFAULT_CONFIG_FILE},
+	// For development and testing only !!!
+	{"disable-serv-check", 'd', NULL,  OPTION_HIDDEN, NULL},
 	{NULL}
 };
+static bool port_once_parsed;
 
-static bool is_accessible(const char *filename) {
+bool is_accessible(const char *filename) {
+	TRACE_FUNC;
 	return (access(filename, R_OK) == 0);
 }
 
-static void verify_access(const char *filename) {
-	if (!is_accessible(filename)) {
-		fprintf(stderr, "%s can't be accessed\n", filename);
-		exit(EXIT_FAILURE);
-	}
+void verify_access(const char *filename) {
+	TRACE_FUNC;
+	if (!is_accessible(filename))
+		critical("%s can't be accessed.", filename);
 }
 
-static error_t parse_opt (int key, char *arg, struct argp_state *state) {
-	struct proxy_conf *conf = state->input;
+void set_field(char **orig_str, size_t *orig_str_mem_len,
+		const char *new_str, size_t new_str_len) {
+	TRACE_FUNC;
+	if ((new_str_len + 1) > *orig_str_mem_len) {
+		*orig_str_mem_len = new_str_len + 1;
+		*orig_str = realloc(*orig_str, *orig_str_mem_len);
+	}
+	memcpy(*orig_str, new_str, new_str_len);
+	(*orig_str)[new_str_len] = '\0';
+}
+
+int parse_port(const char *str) {
+	TRACE_FUNC;
+	char *end_ptr;
+	errno = 0;
+	long result = strtol(str, &end_ptr, 10);
+	if (errno || // conversion error
+			end_ptr == str || // no digits
+			*end_ptr != '\0' || // number in the begining of a text
+			result < 0 || // negative value
+			result > 65335) // max port value
+		return -1;
+	return (int)result;
+}
+
+error_t parse_opt (int key, char *arg, struct argp_state *state) {
+	TRACE_FUNC;
+	int ret = 0;
+	struct proxy_conf *conf = (struct proxy_conf *) state->input;
+	int tmp;
 	switch (key) {
 		case 'S':
-			conf->upstream_srv = arg;
+			set_field(&conf->mqtt_broker, &conf->mqtt_broker_len, arg,
+				strlen(arg));
+			break;
+		case 'p':
+			if ((tmp = parse_port(arg)) == -1) {
+				if (!port_once_parsed)
+					warning("Port has invalid value. Ignoring this option.\nUsing configuration file or further default port instead");
+			} else
+				conf->mqtt_port = tmp;
+			// to don't print warning twice
+			port_once_parsed = true;
 			break;
 		case 's':
-			conf->local_socket = arg;
+			set_field(&conf->zmq_sock_path, &conf->zmq_sock_p_len, arg,
+				strlen(arg));
 			break;
 		case 'c':
-			conf->ca_file = arg;
+			set_field(&conf->ca_cert_file, &conf->ca_cert_f_len, arg,
+				strlen(arg));
 			break;
 		case 'C':
-			conf->client_cert_file = arg;
+			set_field(&conf->mqtt_client_cert_file, &conf->mqtt_cl_cert_f_len,
+				arg, strlen(arg));
 			break;
 		case 'K':
-			conf->client_key_file = arg;
+			set_field(&conf->mqtt_client_key_file, &conf->mqtt_cl_key_f_len,
+				arg, strlen(arg));
 			break;
 		case 't':
-			strncpy(conf->device_token, arg, DEVICE_TOKEN_LEN + 1);
+			set_field(&conf->device_token, &conf->dt_len, arg, strlen(arg));
 			break;
 		case 'f':
-			conf->config_file = arg;
-			conf->custom_conf_file = true;
+			set_field(&conf->config_file, &conf->conf_f_len, arg, strlen(arg));
 			break;
-		case ARGP_KEY_ARG:
-		  if (state->arg_num >= 1)
-			/* Too many arguments. */
-			argp_usage(state);
-		  break;
+		case 'd':
+			conf->disable_serv_check = true;
+			break;
 		default:
-		  return ARGP_ERR_UNKNOWN;
+			ret = ARGP_ERR_UNKNOWN;
+			break;
 		}
-	return 0;
+	return ret;
 }
 
-static void load_cli_opts(int argc, char *argv[], struct proxy_conf *conf) {
+void load_cli_opts(int argc, char *argv[], struct proxy_conf *conf) {
 	// This function might be called multiple times and must be idempotent
-	logc_argp_log = log_sentinel_proxy; // set our log to be configured by logc_argp
+	TRACE_FUNC;
+	// set our log to be configured by logc_argp
+	logc_argp_log = log_sentinel_proxy;
 	struct argp argp = {
 		.options = options,
 		.parser = parse_opt,
@@ -98,45 +158,131 @@ static void load_cli_opts(int argc, char *argv[], struct proxy_conf *conf) {
 		.children = (struct argp_child[]){{&logc_argp_parser, 0, "Logging", 2},
 			{NULL}},
 	};
-
 	argp_parse(&argp, argc, argv, 0, 0, conf);
 }
 
-static void load_config_file(const char *path, struct proxy_conf *conf) {
+void load_conf_str(const config_t *cf, const char *name, char **dest,
+		size_t *dest_mem_len) {
+	TRACE_FUNC;
+	const char *tmp = NULL;
+	if (config_lookup_string(cf, name, &tmp) == CONFIG_TRUE)
+		set_field(dest, dest_mem_len, tmp, strlen(tmp));
+}
+
+void load_config_file(const char *path, struct proxy_conf *conf) {
+	TRACE_FUNC;
 	config_t cfg;
-	const char *tmp="";
 	config_init(&cfg);
-	if(!config_read_file(&cfg, path)){
-		fprintf(stderr, "error reading config file %s: %s:%d - %s\n",
-			path, config_error_file(&cfg), config_error_line(&cfg),
-			config_error_text(&cfg));
+	if(config_read_file(&cfg, path) != CONFIG_TRUE){
+		warning("Wrong syntax in config file: %s on line %d: %s.\nIgnoring config file. Using CLI options or further default configuration instead.",
+			path, config_error_line(&cfg), config_error_text(&cfg));
 		config_destroy(&cfg);
-		exit(EXIT_FAILURE);
+		return;
 	}
-	config_lookup_string(&cfg, "device_token", &tmp);
-	strncpy(conf->device_token, tmp, DEVICE_TOKEN_LEN + 1);
+	load_conf_str(&cfg, CONF_FIELD_DEVICE_TOKEN, &conf->device_token,
+		&conf->dt_len);
+	load_conf_str(&cfg, CONF_FIELD_SEREVR, &conf->mqtt_broker,
+		&conf->mqtt_broker_len);
+	load_conf_str(&cfg, CONF_FIELD_CL_CERT_FILE, &conf->mqtt_client_cert_file,
+		&conf->mqtt_cl_cert_f_len);
+	load_conf_str(&cfg, CONF_FIELD_CL_KEY_FILE, &conf->mqtt_client_key_file,
+		&conf->mqtt_cl_key_f_len);
+	load_conf_str(&cfg, CONF_FIELD_CA_CERT_FILE, &conf->ca_cert_file,
+		&conf->ca_cert_f_len);
+	load_conf_str(&cfg, CONF_FIELD_ZMQ_SOCK_PATH, &conf->zmq_sock_path,
+		&conf->zmq_sock_p_len);
+	config_lookup_int(&cfg, CONF_FIELD_PORT, &conf->mqtt_port);
 	config_destroy(&cfg);
+}
+
+void init_conf(struct proxy_conf *conf) {
+	TRACE_FUNC;
+	assert(conf);
+	conf->disable_serv_check = false;
+	conf->mqtt_port = DEFAULT_PORT;
+
+	conf->dt_len = DEV_TOKEN_MEM_LEN;
+	conf->device_token = malloc(conf->dt_len);
+	conf->device_token[0] = '\0';
+
+	conf->mqtt_broker_len = sizeof(DEFAULT_SERVER);
+	conf->mqtt_broker = malloc(conf->mqtt_broker_len);
+	memcpy(conf->mqtt_broker, DEFAULT_SERVER, conf->mqtt_broker_len);
+
+	conf->zmq_sock_p_len = sizeof(DEFAULT_ZMQ_SOCK_PATH);
+	conf->zmq_sock_path = malloc(conf->zmq_sock_p_len);
+	memcpy(conf->zmq_sock_path, DEFAULT_ZMQ_SOCK_PATH, conf->zmq_sock_p_len);
+
+	conf->ca_cert_f_len = sizeof(DEFAULT_CA_CERT_FILE);
+	conf->ca_cert_file = malloc(conf->ca_cert_f_len);
+	memcpy(conf->ca_cert_file, DEFAULT_CA_CERT_FILE, conf->ca_cert_f_len);
+
+	conf->mqtt_cl_cert_f_len = sizeof(DEFAULT_MQTT_CLIENT_CERT_FILE);
+	conf->mqtt_client_cert_file = malloc(conf->mqtt_cl_cert_f_len);
+	memcpy(conf->mqtt_client_cert_file, DEFAULT_MQTT_CLIENT_CERT_FILE,
+		conf->mqtt_cl_cert_f_len);
+
+	conf->mqtt_cl_key_f_len = sizeof(DEFAULT_MQTT_CLIENT_KEY_FILE);
+	conf->mqtt_client_key_file = malloc(conf->mqtt_cl_key_f_len);
+	memcpy(conf->mqtt_client_key_file, DEFAULT_MQTT_CLIENT_KEY_FILE,
+		conf->mqtt_cl_key_f_len);
+
+	conf->conf_f_len = sizeof(DEFAULT_CONFIG_FILE);
+	conf->config_file = malloc(conf->conf_f_len);
+	memcpy(conf->config_file, DEFAULT_CONFIG_FILE, conf->conf_f_len);
 }
 
 void load_conf(int argc, char *argv[], struct proxy_conf *conf) {
 	// We load cli params first (to get config file path most notably) Then we
 	// load config file if exists end is readable. If that is succesfull we have
 	// to load cli params once more - since they have higher priority.
+	TRACE_FUNC;
+	assert(argc > 0);
+	assert(argv);
+	assert(conf);
+	port_once_parsed = false;
 	load_cli_opts(argc, argv, conf);
 	if (is_accessible(conf->config_file)) {
 		load_config_file(conf->config_file, conf);
 		load_cli_opts(argc, argv, conf);
-	} else {
-		fprintf(stderr, "WARN: config file %s can't be accessed\n",
+	} else
+		warning("Config file %s can't be accessed.\nUsing CLI options or further default configuration instead.",
 			conf->config_file);
+	enum dt_state verify_status = device_token_verify(conf->device_token);
+	if (verify_status != DT_OK)
+		if (verify_status == DT_UNDEF)
+			critical("Sentinel Device token must be specified\nUse CLI options or configuartion for that.");
+		else
+			critical("Failed to verify Sentinel Device Token: %s",
+				device_token_state_msg(verify_status));
+	verify_access(conf->mqtt_client_cert_file);
+	verify_access(conf->mqtt_client_key_file);
+	if (!conf->disable_serv_check)
+		// we need CA cert only if server check is enabled
+		verify_access(conf->ca_cert_file);
+	info("Sentinel Device token check passed");
+	info("Using following configuration:");
+	info("Sentinel server: %s", conf->mqtt_broker);
+	info("Sentinel server port: %d", conf->mqtt_port);
+	info("ZMQ socket: %s", conf->zmq_sock_path);
+	info("Sentinel client certificate: %s", conf->mqtt_client_cert_file);
+	info("Sentinel client key: %s", conf->mqtt_client_key_file);
+	if (conf->disable_serv_check)
+		warning("Sentinel server verification disabled!!!");
+	else
+		// we need CA cert only if server check is enabled
+		info("Sentinel CA certificate: %s", conf->ca_cert_file);
+}
+
+void destroy_conf(struct proxy_conf *conf) {
+	TRACE_FUNC;
+	if (conf) {
+		free(conf->device_token);
+		free(conf->mqtt_broker);
+		free(conf->zmq_sock_path);
+		free(conf->ca_cert_file);
+		free(conf->mqtt_client_cert_file);
+		free(conf->mqtt_client_key_file);
+		free(conf->config_file);
 	}
-	int verify_status = device_token_verify(conf->device_token);
-	fprintf(stderr, "%s\n", device_token_state_msg(verify_status));
-	if (verify_status)
-		exit(EXIT_FAILURE);
-	verify_access(conf->ca_file);
-	verify_access(conf->client_cert_file);
-	verify_access(conf->client_key_file);
-	fprintf(stderr, "Using:\nCA cert: %s\nclient cert: %s\nclient private key: %s\n",
-		conf->ca_file, conf->client_cert_file, conf->client_key_file);
 }
--- proxy/proxy_conf.h
+++ proxy/proxy_conf.h
@@ -1,6 +1,6 @@
 /*
  *  Turris:Sentinel Proxy - Main MQTT gateway to Sentinel infrastructure
- *  Copyright (C) 2020 CZ.NIC z.s.p.o. (https://www.nic.cz/)
+ *  Copyright (C) 2018 - 2021 CZ.NIC z.s.p.o. (https://www.nic.cz/)
  *
  *  This program is free software: you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
@@ -20,19 +20,71 @@
 #define __SENTINEL_PROXY_PROXY_CONF_H__
 
 #include <stdbool.h>
+#include <string.h>
 #include <device_token.h>
 
+#define DEV_TOKEN_MEM_LEN (DEVICE_TOKEN_LEN + 1)
+
 struct proxy_conf {
-	const char *upstream_srv;
-	const char *local_socket;
-	const char *ca_file;
-	const char *client_cert_file;
-	const char *client_key_file;
-	char device_token[DEVICE_TOKEN_LEN + 1];
-	const char *config_file;
-	bool custom_conf_file;
+	bool disable_serv_check;
+	int mqtt_port;
+	char *mqtt_broker;
+	size_t mqtt_broker_len;
+	char *mqtt_client_cert_file;
+	size_t mqtt_cl_cert_f_len;
+	char *mqtt_client_key_file;
+	size_t mqtt_cl_key_f_len;
+	char *ca_cert_file;
+	size_t ca_cert_f_len;
+	char *zmq_sock_path;
+	size_t zmq_sock_p_len;
+	char *config_file;
+	size_t conf_f_len;
+	char *device_token;
+	size_t dt_len;
 };
 
+// NOTE: This is private API exposed just for the testing.
+// It is NOT supposed to be used anywhere else.
+
+// Converts C NULL terminated string representing port number (0 - 65535) to integer value .
+// If string contains invalid port value it returns -1.
+int parse_port(const char *str);
+
+// Copies new_strl_len bytes of new_str to *orig_str and puts NULL terminating
+// char at the end of new_str. In case new_str_len + 1 is bigger than
+// *orig_str_mem_len then *orig_str is reallocated to new_str_len + 1
+// using realloc() and *orig_str_mem_len is updated accordingly.
+// Before first use on any *orig_str, its memory MUST be first allocated
+// by malloc() and *orig_str_mem_len initialized accordingly.
+void set_field(char **orig_str, size_t *orig_str_mem_len, const char *new_str,
+	size_t new_str_len);
+
+// Loads configuration from CLI options and their arguments.
+// conf struct MUST be first properly initialized by init_conf().
+void load_cli_opts(int argc, char *argv[], struct proxy_conf *conf);
+
+// Loads configuration from given configuration file.
+// conf struct MUST be first properly initialized by init_conf().
+void load_config_file(const char *path, struct proxy_conf *conf);
+
+
+// NOTE: This is public API intended for normal use.
+
+// Allocates memory for conf struct fields and initializes them with default values.
+// DOES assert check for conf.
+void init_conf(struct proxy_conf *conf);
+
+// Loads configuration from CLI options and configuration file.
+// conf struct MUST be first properly initialized by init_conf().
+// The priorities of configuration is following:
+// CLi options > configuration file > default conf
+// CLI options have higher priority than conf. file, which has higher priority
+// than default configuration.
+// DOES assert check for conf, argv and argc.
 void load_conf(int argc, char *argv[], struct proxy_conf *conf);
+
+// If conf is NOT NULL frees all the memory hold by conf struct fields.
+void destroy_conf(struct proxy_conf *conf);
 
 #endif /*__SENTINEL_PROXY_PROXY_CONF_H__*/

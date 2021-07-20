ac532fda14130b67af0fca94df43df8b595b6697
Miroslav Hanak <miroslav.hanak@nic.cz>
2021-07-20 17:14:06 +0200

    device_token: update library header file
    * improved description in license header
    * indent using tabs
    * removed useless header include
    * improved doc comments

--- include/device_token.h
+++ include/device_token.h
@@ -1,6 +1,6 @@
 /*
- *  Turris:Sentinel Proxy - Main MQTT gateway to Sentinel infrastructure
- *  Copyright (C) 2020 CZ.NIC z.s.p.o. (https://www.nic.cz/)
+ *  Turris:Sentinel Device Token - Device Token management library
+ *  Copyright (C) 2020 - 2021 CZ.NIC z.s.p.o. (https://www.nic.cz/)
  *
  *  This program is free software: you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
@@ -16,38 +16,39 @@
  *  along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
 
-#ifndef __SENTINEL_PROXY_DEVICE_TOKEN_H__
-#define __SENTINEL_PROXY_DEVICE_TOKEN_H__
-#include <stdbool.h>
+#ifndef __SENTINEL_DEVICE_TOKEN_H__
+#define __SENTINEL_DEVICE_TOKEN_H__
 
 #define DEVICE_TOKEN_LEN 64
 
 enum dt_state {
-    DT_OK,
-    DT_UNDEF,  // device token not provided
-    DT_LENGTH,  // token length does not match
-    DT_DECODE,  // failure in decoding token
-    DT_CRC,  // token crc does not match
-    DT_NUM_STATES,  // guard value
+	DT_OK,
+	DT_UNDEF,  // provided token is NULL
+	DT_LENGTH,  // token length does not match
+	DT_DECODE,  // token contains invalid characters
+	DT_CRC,  // token crc does not match
+	DT_NUM_STATES,  // guard value
 };
 
 /*
- * Return message describing provided state.
+ * Returns message string describing provided state or NULL if the state
+ * is unknown. Returned string is staticaly allocated and MUST NOT be freed.
  */ 
 const char *device_token_state_msg(enum dt_state state);
 
 /*
- * Generate DEVICE_TOKEN_LEN hexachars long string with embedded CRC.
+ * Generates DEVICE_TOKEN_LEN hexachars long string with embedded CRC.
  * On success return the string, otherwise return NULL.
+ * Caller is responsible for freeing the string.
  */
 char *device_token_generate() __attribute__((malloc));
 
 /*
- * Verify device_token format and CRC. Return dt_state code which
+ * Verifies device_token format and CRC. Return dt_state code which
  * equals DT_OK on success or one of the error codes otherwise.
  * device_token is supposed to be DEVICE_TOKEN_LEN hexachars long
  * string with embedded CRC.
  */
 enum dt_state device_token_verify(const char *device_token);
 
-#endif /*__SENTINEL_PROXY_DEVICE_TOKEN_H__*/
+#endif /*__SENTINEL_DEVICE_TOKEN_H__*/
